67d76cd6bda290560edc6ac828e7177bfd6914db
Blake Knight <oss.ideas@gmail.com>
2020-12-16 22:44:52 -0600

    :sparkles: rewrite in Go
    - fixes notifications not working on macOS
    - shows time the timer completed
    - the `--custom` flag has been changed from accepting time in minutes (e.g. `15`) to accepting a duration string (e.g. `1h2m3s`)
    - changes from using `time.Sleep()` to `time.Ticker`

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/blakek/pomodoro
 go 1.14
 
 require (
+	github.com/blakek/go-notifier v0.1.0
 	github.com/jessevdk/go-flags v1.4.0
-	github.com/teddywing/go-notifier v0.0.0-20200310234431-5e1b28caa8d8
 )
--- pomodoro.go
+++ pomodoro.go
@@ -2,54 +2,73 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 
+	"github.com/blakek/go-notifier"
 	"github.com/jessevdk/go-flags"
-	"github.com/teddywing/go-notifier"
 )
 
 var arguments struct {
-	CustomMinutes float64 `long:"custom" short:"c" description:"start a custom timer for given number of minutes" default:"25"`
-	RunShortTimer bool    `long:"short" short:"s" description:"starts a timer for 5 minutes"`
-	RunLongTimer  bool    `long:"long" short:"l" description:"starts a timer for 20 minutes"`
+	CustomTimer   string `long:"custom" short:"c" description:"start a custom timer for given number of minutes" default:"25m"`
+	RunShortTimer bool   `long:"short" short:"s" description:"starts a timer for 5 minutes"`
+	RunLongTimer  bool   `long:"long" short:"l" description:"starts a timer for 20 minutes"`
 }
 
-func printTime(time int) {
-	minutes := time / 60
-	seconds := time % 60
+func clearCurrentLine() {
+	fmt.Print("\033[2K\r")
+}
 
-	fmt.Printf("\033[2K\r%v:%02v remaining", minutes, seconds)
+func printTime(timeRemaining time.Duration) {
+	clearCurrentLine()
+
+	if timeRemaining <= 0 {
+		fmt.Printf("Timer finished at %s\n", time.Now().Format(time.Kitchen))
+	} else {
+		fmt.Printf("%s remaining", timeRemaining.String())
+	}
 }
 
 func main() {
-	var minutes float64
-
+	var duration time.Duration
 	flags.Parse(&arguments)
 
 	if arguments.RunLongTimer {
-		minutes = 20.0
+		duration = 20 * time.Minute
 	} else if arguments.RunShortTimer {
-		minutes = 5.0
+		duration = 5 * time.Minute
 	} else {
-		minutes = arguments.CustomMinutes
+		var parseError error
+		duration, parseError = time.ParseDuration(arguments.CustomTimer)
+
+		if parseError != nil {
+			fmt.Fprintln(os.Stderr, parseError)
+			os.Exit(2)
+		}
+
 	}
 
-	seconds := int(minutes * 60)
+	ticker := time.NewTicker(time.Second)
+	timeRemaining := duration
 
-	for i := seconds; i > 0; i-- {
-		printTime(i)
-		time.Sleep(1 * time.Second)
+	for range ticker.C {
+		timeRemaining = (timeRemaining - time.Second).Round(time.Second)
+		printTime(timeRemaining)
+
+		if timeRemaining <= 0 {
+			break
+		}
 	}
-
-	// Print final newline
-	fmt.Println()
 
 	notification := notifier.Notification{
-		Title:   "This is the title",
-		Message: "This is the message",
+		Title:   "Timer Finished",
+		Message: fmt.Sprintf("Finished %s timer", duration.String()),
+		Timeout: 15,
 	}
 
 	systemNotifier, _ := notifier.NewNotifier()
-
 	systemNotifier.DeliverNotification(notification)
+
+	// HACK: tries to deliver notification before exiting
+	time.Sleep(time.Second)
 }
